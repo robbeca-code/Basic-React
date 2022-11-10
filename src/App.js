896733f040b70da89860a8ca37195698acab63ff
robbeca <xmfkddl1006@gmail.com>
2022-11-11 00:15:05 +0900

    Create the counter page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@
 // import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
+import Counter from './pages/Counter';
 import {Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="/About">About</Link>
+        <Link to="/">Home</Link> | <Link to="/About">About</Link> | <Link to="/Counter">Counter</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/About" element={<About />}/>
+        <Route path="/Counter" element={<Counter />}/>
       </Routes>
     </div>
   );
