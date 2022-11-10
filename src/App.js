cb80fbeba1224c05861611be24bf1f28d6ac1c17
robbeca <xmfkddl1006@gmail.com>
2022-11-11 00:04:19 +0900

    Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import {Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/About">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/About" element={<About />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// function Home() {
+//   return <h1>Hi nice to meet you!!</h1>;
+// }
+const Home = () => {
+  return <h1>This is the Home page</h1>;
+}
+
+export default Home;
